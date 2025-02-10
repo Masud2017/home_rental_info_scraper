c153217d3c4caf8f0bf47f018055478139586997
Md Masud karim <msmasud578@gmail.com>
2025-02-10 22:29:18 +0100

    Aded scraping logic for frieslandhuurt

--- home_rental_info_scraper/spiders/frieslandhuurt.py
+++ home_rental_info_scraper/spiders/frieslandhuurt.py
@@ -1,10 +1,44 @@
 import scrapy
-
+import asyncio
+from scrapy_playwright.page import PageMethod
+from scrapy.selector import Selector
 
 class FrieslandhuurtSpider(scrapy.Spider):
     name = "frieslandhuurt"
     allowed_domains = ["www.frieslandhuurt.nl"]
-    start_urls = ["https://www.frieslandhuurt.nl/aanbod/nu-te-huur/huurwoningen/details/"]
+    start_urls = ["https://www.frieslandhuurt.nl/aanbod/nu-te-huur/huurwoningen"]
 
-    def parse(self, response):
-        pass
+    def start_requests(self):
+        yield scrapy.Request(
+            url=self.start_urls[0],
+            meta={
+                "playwright": True,
+                "playwright_include_page": True,
+                "playwright_page_methods": [
+                    PageMethod("wait_for_selector", "div.list-item-content", timeout=6000)
+                ],
+                # "page_number": 1
+            },
+            # headers ={}
+        )
+
+    async def parse(self, response):
+        page = response.meta["playwright_page"]
+        
+        data = await page.content()
+        home_card_list = Selector(text=data).xpath("//div[contains(@class, 'list-item-content')]")
+        print(len(home_card_list))
+        for home_card in home_card_list:
+            image_link = self.allowed_domains[0] + home_card.xpath(".//img/@src").get()
+            street = home_card.xpath(".//div[contains(@class, 'object-address')]/span[1]/span/text()").get()
+            if street is None:
+                street = ""
+            address = street + home_card.xpath(".//div[contains(@class, 'object-address')]/span[1]/text()").get()
+            price = home_card.xpath(".//span[contains(@class, 'kosten-regel2')]/text()").get()
+            url = self.allowed_domains[0] + home_card.xpath(".//a[contains(@ng-click,'goToDetails')]").attrib['href']
+            
+            print(f"Image : {image_link}")
+            print(f"Price : {price}")
+            print(f"Address : {address}")
+            print(f"URL : {url}")
+            
