bb3de8d2b56d7862a0b80e87fbb1752cff9f4b3b
Md Masud karim <msmasud578@gmail.com>
2025-02-10 22:51:39 +0100

    Added base spider to the hwwonen spider

--- home_rental_info_scraper/spiders/hwwonen.py
+++ home_rental_info_scraper/spiders/hwwonen.py
@@ -1,10 +1,8 @@
 import scrapy
+from .similar_web_scrapper import SimilarWebScrapper
 
-
-class HwwonenSpider(scrapy.Spider):
+class HwwonenSpider(SimilarWebScrapper):
     name = "hwwonen"
     allowed_domains = ["www.thuisbijhwwonen.nl"]
-    start_urls = ["https://www.thuisbijhwwonen.nl/aanbod/nu-te-huur/huurwoningen/details/"]
-
-    def parse(self, response):
-        pass
+    start_urls = ["https://www.thuisbijhwwonen.nl/aanbod/nu-te-huur/huurwoningen"]
+    
