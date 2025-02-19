7b6e010f57944d39c1c8f88790df5750814e66a8
Md Masud karim <msmasud578@gmail.com>
2025-02-19 22:33:45 +0100

    updated the similar_web_scrapper.py file

--- home_rental_info_scraper/spiders/similar_web_scrapper.py
+++ home_rental_info_scraper/spiders/similar_web_scrapper.py
@@ -2,6 +2,8 @@ import scrapy
 import asyncio
 from scrapy_playwright.page import PageMethod
 from scrapy.selector import Selector
+from home_rental_info_scraper.models.Home import Home
+from ..items import HomeRentalInfoScraperItem
 
 class SimilarWebScrapper(scrapy.Spider):
     def start_requests(self):
@@ -25,7 +27,7 @@ class SimilarWebScrapper(scrapy.Spider):
         home_card_list = Selector(text=data).xpath("//div[contains(@class, 'list-item-content')]")
         print(len(home_card_list))
         for home_card in home_card_list:
-            image_link = self.allowed_domains[0] + home_card.xpath(".//img/@src").get()
+            image_url = self.allowed_domains[0] + home_card.xpath(".//img/@src").get()
             street = home_card.xpath(".//div[contains(@class, 'object-address')]/span[1]/span/text()").get()
             address = street + home_card.xpath(".//div[contains(@class, 'object-address')]/span[1]/text()").get()
             city = home_card.xpath(".//div[contains(@class, 'object-address')]//span[contains(@class, 'address-part')][2]/text()").get()
@@ -33,10 +35,20 @@ class SimilarWebScrapper(scrapy.Spider):
             price = home_card.xpath(".//span[contains(@class, 'kosten-regel2')]/text()").get()
             url = self.allowed_domains[0] + home_card.xpath(".//a[contains(@ng-click,'goToDetails')]").attrib['href']
             
-            print(f"Image : {image_link}")
+            print(f"Image : {image_url}")
             print(f"Price : {price}")
             print(f"Address : {address}")
             print(f"URL : {url}")
             print(f"City : {city}")
             print(f"Agency : {agency}")            
+            
+            home = Home(
+                image_url=image_url,
+                address=address,
+                city=city,
+                agency=agency,
+                price=price,
+                url=url
+            )
+            yield HomeRentalInfoScraperItem(home=home)
     
