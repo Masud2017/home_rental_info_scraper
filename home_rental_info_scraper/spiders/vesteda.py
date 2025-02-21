30435635770a8ce224dd4fd06504b43cbe4f69cb
Md Masud karim <msmasud578@gmail.com>
2025-02-21 11:05:23 +0100

    updated the vesteda spider

--- home_rental_info_scraper/spiders/vesteda.py
+++ home_rental_info_scraper/spiders/vesteda.py
@@ -2,6 +2,8 @@ import scrapy
 import asyncio
 from scrapy_playwright.page import PageMethod
 from scrapy.selector import Selector
+from home_rental_info_scraper.models.Home import Home
+from home_rental_info_scraper.items import HomeRentalInfoScraperItem
 
 class VestedaSpider(scrapy.Spider):
     name = "vesteda"
@@ -40,27 +42,29 @@ class VestedaSpider(scrapy.Spider):
             address = home_card.xpath(".//div[contains(@class, 'o-card--listview-content')]//h3[contains(@class, 'h4 u-margin-bottom-none')]/span/text()").get() + ","+ city
             price = home_card.xpath(".//div[contains(@class, 'o-card--listview-content')]//div[contains(@class, 'o-card--listview-price')]/b[contains(@class, 'h5')]/text()").get()
             agency = "Vesteda"
+            room_count = home_card.xpath(".//ul[contains(@class, 'o-layout o-layout-gap o-layout--gutter-tiny')]/li[2]/b/text()").get()
+            if room_count is not None:
+                room_count = room_count.strip()
+                room_count = room_count.split(" ")[0]
+            else:
+                room_count = "1"
             # date_added = home_card.xpath(".//div[contains(@class, 'o-card--listview-content')]//div[contains(@class, 'o-card--listview-price')]/text()").get()
             
-            print(url)
+            print(f"url : {url}")
             print(f"image url : {image_url}")
             print(f"city : {city}")
             print(f"address: {address}")
             print(f"price : {price}")
             print(f"agency : {agency}")
+            print(f"room_count : {room_count}")
             
-    
-    
-# home_card_list = Selector(text=data).xpath("//div[contains(@class, 'list-item-content')]")
-# print(len(home_card_list))
-# for home_card in home_card_list:
-#     image_link = self.allowed_domains[0] + home_card.xpath(".//img/@src").get()
-#     street = home_card.xpath(".//div[contains(@class, 'object-address')]/span[1]/span/text()").get()
-#     address = street + home_card.xpath(".//div[contains(@class, 'object-address')]/span[1]/text()").get()
-#     price = home_card.xpath(".//span[contains(@class, 'kosten-regel2')]/text()").get()
-#     url = self.allowed_domains[0] + home_card.xpath(".//a[contains(@ng-click,'goToDetails')]").attrib['href']
-    
-#     print(f"Image : {image_link}")
-#     print(f"Price : {price}")
-#     print(f"Address : {address}")
-#     print(f"URL : {url}")
+            home = Home(
+                    address=address,
+                    city=city,
+                    url=url,
+                    agency=agency,
+                    price=price,
+                    image_url=image_url,
+                    room_count=room_count
+                )
+            yield HomeRentalInfoScraperItem(home=home)
