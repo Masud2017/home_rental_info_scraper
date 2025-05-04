ec8f820b2f48b01a19b569a4adce0e7139194abc
Md Masud karim <msmasud578@gmail.com>
2025-05-04 22:42:40 +0200

    Added unit test for home_services/home_service.py::existIn() method

--- home_rental_info_scraper/tests/home_service_test.py
+++ home_rental_info_scraper/tests/home_service_test.py
@@ -1,5 +1,5 @@
 import unittest
-from home_rental_info_scraper.services.home_services import send_whatsapp_notification_on_user_preferences,sanitize_string
+from home_rental_info_scraper.services.home_services import send_whatsapp_notification_on_user_preferences,sanitize_string,exitsIn
 from home_rental_info_scraper.models.Home import Home
 
 class HomeServiceTest(unittest.TestCase):
@@ -19,8 +19,38 @@ class HomeServiceTest(unittest.TestCase):
     #     )
     #     send_whatsapp_notification_on_user_preferences([home])
         
-    def test_sanitize_string(self):
-        test_string = "Appartement \'s-Gravenweg,Rotterdam"
-        expected_output = "Appartement s-Gravenweg,Rotterdam"
-        result = sanitize_string(test_string)
-        self.assertEqual(result, expected_output)
+    # def test_sanitize_string(self):
+    #     test_string = "Appartement \'s-Gravenweg,Rotterdam"
+    #     expected_output = "Appartement s-Gravenweg,Rotterdam"
+    #     result = sanitize_string(test_string)
+    #     self.assertEqual(result, expected_output)
+    def test_exists_in(self):
+        home11 = Home(
+            address="123 Main St",
+            city="Amsterdam",
+            url="http://example.com",
+            agency="Example Agency",
+            price="150",
+            image_url="http://example.com/image.jpg",
+            room_count="2"
+        )
+        home ={
+            "address":"123 Main St",
+            "city":"Amsterdam",
+            "url":"http://example.com",
+            "agency":"Example Agency",
+            "price":"150",
+            "image_url":"http://example.com/image.jpg",
+            "room_count":"2"
+        }
+        home2 = {
+            "address":"3453 albert St",
+            "city":"Dan heg",
+            "url":"http://example.com",
+            "agency":"Example Agency",
+            "price":"1150",
+            "image_url":"http://example.com/image.jpg",
+            "room_count":"12"
+        }
+        test_house_list = [home, home2]
+        self.assertTrue(exitsIn(home11,test_house_list))
